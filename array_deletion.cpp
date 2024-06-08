a9f659e1ed007f1d42c4e66f5b9de30d21090d77
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2024-06-08 12:39:54 +0530

    created deletion

---
+++ array_deletion.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int size;
+int* arr;
+
+void delete_index(int index)
+{
+    for(int i=index;i<size;i++)
+    {
+        arr[index]=arr[index+1];
+    }
+    size--;
+}
+
+int main()
+{
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    arr=new int[size];
+
+    cout<<"Enter the elements of array: ";
+    int i=0;
+    while(i<size)
+    {
+        cin>>arr[i];
+        i++;
+    }
+    int index;
+    cout<<"Enter the index to delete: ";
+    cin>>index;
+
+    delete_index(index);
+
+    cout<<"Array after deletion: ";
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
--- array_insertion.cpp
+++ array_insertion.cpp
@@ -1,36 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void insert_element(int element,int index)
+int size;
+int* arr;
+
+void insert_index(int element, int index)
 {
-    for(int i=n;i>index;i--)
-    {
-        arr[i]=arr[i-1];
-    }
-    arr[index]=element;
-    size++;
+	//Shift the existing elements
+	for(int i= size; i > index; i--)
+		arr[i] = arr[i-1];
+		
+	//put the element at index 
+	
+	arr[index] = element;
+	size++;	//update the number of elements in the array
 }
+
+
 int main()
 {
-    int size;
-    cout<<"Enter the size of array: ";
-    cin>>size;
 
+	cout<<"Enter the size of Array: ";
+	cin>> size;	
+	
 
-    int* arr; 
-    arr=new int[size];
-
-    cout<<"Enter the elements of array: ";
-    int x=1;
-    while(x<size+1)
-    {
-        cin>>arr[x-1];
-        x++;
-    }
-
-    for(int i=0;i<size;i++)
-    {
-        cout<<arr[i]<<endl;
-    }
-return 0;
+	arr = new int[size];	//Creating Array
+	
+	//	int* arr = new int[size];
+	
+	//Print the values of Array
+	
+	cout<<"\nEnter the Values of Array\n";
+	
+	int x = 1;
+	while(x <= size)
+	{
+		cin >> arr[x-1];	
+		x++;
+	}
+	int element,index;
+    cout<<"Enter the element: ";
+    cin>>element;
+    cout<<"Enter index to insert: ";
+    cin>>index;
+    insert_index(element,index);
+	
+	for(int i=0; i < size; i++)
+		cout<<arr[i] <<"\n";
+	
+	
+	return 0;
 }
