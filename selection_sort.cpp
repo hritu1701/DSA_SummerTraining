ce741d6b515c25d80b3470f0af4709e5931308c4
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2024-06-16 10:13:49 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,6 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void selection_sort(int* arr,int size)
+{
+    for(int i=0; i< size-1; i++) 
+	{
+		int min = i;
+		for(int j= i+1; j<size; j++)
+		   {
+		   	 if(arr[j] < arr[min]) 
+             min = j;
+		   }
+		if(i != min)
+		{
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+}
+
 
 int main()
 {
